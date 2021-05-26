b93e12017eae54a9399f22e5c4a7a7ba26a67017
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-05-26 04:06:46 -0400

    commit tree 44e3b4b5f8e89546ffb643c303c1f5d472f7dd15

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,30 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+
+    state = {
+      reviews: []
+    }
+  
+    componentDidMount = () => {
+      fetch(URL)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ reviews: data.results })
+        console.log(data.results)
+      })
+    }
+  
+  render() {
+    return (
+      <div className='latest-movie-reviews'>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+      )
+    }
+  
+  }
+  
+  export default LatestMovieReviewsContainer
